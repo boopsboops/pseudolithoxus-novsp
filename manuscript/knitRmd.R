eb0ca3fc7e9959fd829e424da4a5cdc216618774
rupert@sorubim <rupertcollins@gmail.com>
2014-12-13 15:55:34 -0800

    moved the lib calls into the command file

--- manuscript/knitRmd.R
+++ manuscript/knitRmd.R
@@ -1,12 +1,14 @@
 # load libs
 require(knitr)
-require(markdown)
 require(rmarkdown)
+require(ape)
+require(phangorn)
+require(spider)
+
 # first need to run 'sudo apt-get install pandoc-citeproc'
 
-#cd
+# cd
 setwd("/home/rupert/Projects/pseudolithoxus-novsp/manuscript")
 
 # run the R code in knitr and render the document in HTML format
-knit("manuscript-master.Rmd") # ?knit
 render("manuscript-master.Rmd", "html_document") #?render
