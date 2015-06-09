fc00d00e1366eb207e60bcbf4fcced14167dab19
rupert@sorubim <rupertcollins@gmail.com>
2015-06-09 00:23:01 -0400

    wrote code to download sequences from nathans paper and build a tree

--- analyses/tree.R
+++ analyses/tree.R
@@ -1,9 +1,63 @@
 # start the chunk
 ## @knitr treecode
 
-# download seqs from Lujan et al. 2014
-# testing with some Rhinichthys
+require("rentrez")
+require("ape")
+require("phangorn")
+require("phyloch")
+require("phytools")
 
+# search entrez
+# note: there are other Cramer seqs available. Need to refine search if we want these
+rs <- entrez_search(db="nuccore", term="(Pseudolithoxus[Organism] OR Soromonichthys[Organism] OR Lasiancistrus[Organism]) AND (cytb[Gene Name] OR RAG1[Gene Name])", retmax=100)
+# run to check all is okay
+es <- entrez_summary(db="nuccore", id=rs$ids)
+sapply(es, "[[", "title")
+ess <- sapply(es, "[[", "title")
+
+# download the seqs and write to disk
+cytb <- entrez_fetch(db="nuccore", rettype='fasta', id=rs$ids[grep("cytb", ess)])
+rag1 <- entrez_fetch(db="nuccore", rettype='fasta', id=rs$ids[grep("RAG1", ess)])
+write(cytb, "../temp/cytb_raw.fasta")
+write(rag1, "../temp/rag1_raw.fasta")
+
+# load up from disk
+cytbrw <- read.dna("../temp/cytb_raw.fasta", format="fasta", as.matrix=FALSE)
+rag1rw <- read.dna("../temp/rag1_raw.fasta", format="fasta", as.matrix=FALSE)
+rag1rw <- rag1rw[-grep("guapore", names(rag1rw))]#remove 'guapore'
+
+# clean names
+names(cytbrw) <- paste(sapply(strsplit(names(cytbrw), split="\\|"), function(x) x[4]), sapply(strsplit(names(cytbrw), split=" "), function(x) paste(x[2:3], collapse="_")), sep="_")
+names(rag1rw) <- paste(sapply(strsplit(names(rag1rw), split="\\|"), function(x) x[4]), sapply(strsplit(names(rag1rw), split=" "), function(x) paste(x[2:3], collapse="_")), sep="_")
+
+# align the seqs
+cytbAl <- mafft(x=cytbrw, path="mafft")
+rag1Al <- mafft(x=rag1rw, path="mafft")
+
+dat <- cytbAl
+dat <- rag1Al
+
+# make a tree
+prat <- pratchet(as.phyDat(dat), rearrangements="SPR")
+pars <- acctran(prat, as.phyDat(dat))
+mlm <- pml(pars, as.phyDat(dat), k=4, inv=0, model="GTR") 
+mlik <- optim.pml(mlm, optNni=TRUE, optGamma=TRUE, optEdge=TRUE, optBf=TRUE, optQ=TRUE, optInv=FALSE)
+tr <- mlik$tree# rename tree
+outgr <- grep("Lasiancistrus", tr$tip.label, value=TRUE)
+rnod <- fastMRCA(tr, outgr[1], outgr[2])
+rtr <- reroot(tr, node.number=rnod, position=0.5*tr$edge.length[which(tr$edge[,2]==rnod)])
+
+cytbtr <- rtr
+rag1tr <- rtr
+
+# plot and take a look
+par(mfrow=c(1,2))
+plot.phylo(ladderize(cytbtr), direction="rightwards")
+plot.phylo(ladderize(rag1tr), direction="leftwards")
+
+
+
+### old
 # grab data from GenBank
 seqs <- read.GB(access.nb=paste("KF", 640094:640157, sep=""))
 
