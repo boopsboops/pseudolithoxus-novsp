7afc5328e965600c9410065637860116ff76470a
rupert@sorubim <rupertcollins@gmail.com>
2016-07-20 17:46:40 +0100

    updated and reran analyses

--- scripts/species_delimitation.R
+++ scripts/species_delimitation.R
@@ -5,51 +5,54 @@ require("phangorn")
 # rm(list=ls())
 
 ## load data
-cytb <- as.matrix(as.DNAbin(read.nexus.data(file="../temp/final_alignments/cytb.nex")))
-rag1 <- as.matrix(as.DNAbin(read.nexus.data(file="../temp/final_alignments/rag1.nex")))
-ttab <- read.table(file="mol_samples.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
+cytb <- as.matrix(as.DNAbin(read.nexus.data(file="../data/cytb_aligned.nex")))
+rag1 <- as.matrix(as.DNAbin(read.nexus.data(file="../data/rag1_aligned.nex")))
+ttab <- read.table(file="../data/mol_samples.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 
 
 ## set up alternative species delimitation scenarios
 nic12 <- ttab # nicoi gr1+gr2
-nic12$species <- gsub("nicoi.gr1|nicoi.gr2", "nicoi.gr1+gr2", nic12$species)
+nic12$delimitation <- gsub("nicoi.gr1|nicoi.gr2", "nicoi.gr1+gr2", nic12$delimitation)
 nic1nsp <- ttab # nicoi gr1+nsp
-nic1nsp$species <- gsub("nicoi.gr1|n. sp.", "nicoi.gr1+n.sp.", nic1nsp$species)
+nic1nsp$delimitation <- gsub("nicoi.gr1|n.sp.", "nicoi.gr1+n.sp.", nic1nsp$delimitation)
 nic12nsp <- ttab # nicoi gr1+gr2+nsp
-nic12nsp$species <- gsub("nicoi.gr1|nicoi.gr2|n. sp.", "nicoi.gr1+gr2+n.sp.", nic12nsp$species)
+nic12nsp$delimitation <- gsub("nicoi.gr1|nicoi.gr2|n.sp.", "nicoi.gr1+gr2+n.sp.", nic12nsp$delimitation)
 
 
 ## seg sites analysis
 # separate Pseudolithoxus genus and just the nicoi group
-cytbpseud <- cytb[na.omit(match(ttab$code[grep("Pseudolithoxus", ttab$genus)], labels(cytb))), ]
-rag1pseud <- rag1[na.omit(match(ttab$code[grep("Pseudolithoxus", ttab$genus)], labels(rag1))), ]
-cytbnic <- cytb[na.omit(match(ttab$code[grep("nicoi.gr2|nicoi.gr1|n. sp.", ttab$species)], labels(cytb))), ]
-rag1nic <- rag1[na.omit(match(ttab$code[grep("nicoi.gr2|nicoi.gr1|n. sp.", ttab$species)], labels(rag1))), ]
+#cytbpseud <- cytb[na.omit(match(ttab$catalogNumber[grep("Pseudolithoxus", ttab$genus)], labels(cytb))), ]
+#rag1pseud <- rag1[na.omit(match(ttab$catalogNumber[grep("Pseudolithoxus", ttab$genus)], labels(rag1))), ]
+
+cytbnic <- cytb[na.omit(match(ttab$catalogNumber[grep("nicoi.gr2|nicoi.gr1|n.sp.", ttab$delimitation)], labels(cytb))), ]
+rag1nic <- rag1[na.omit(match(ttab$catalogNumber[grep("nicoi.gr2|nicoi.gr1|n.sp.", ttab$delimitation)], labels(rag1))), ]
+
 # make the species vectors (note that this writes over prev objects so run one at a time)
-cytbspp <- ttab$species[match(labels(cytbpseud), ttab$code)]# all pseudos: 3 spp. delim
-rag1spp <- ttab$species[match(labels(rag1pseud), ttab$code)]# all pseudos:3 spp. delim
+#cytbspp <- ttab$delimitation[match(labels(cytbpseud), ttab$catalogNumber)]# all pseudos: 3 spp. delim
+#rag1spp <- ttab$delimitation[match(labels(rag1pseud), ttab$catalogNumber)]# all pseudos:3 spp. delim
 #
-cytbspp <- nic12$species[match(labels(cytbpseud), nic12$code)]# all pseudos: nicoi gr1+gr2
-rag1spp <- nic12$species[match(labels(rag1pseud), nic12$code)]# all pseudos: nicoi gr1+gr2
+#cytbspp <- nic12$delimitation[match(labels(cytbpseud), nic12$catalogNumber)]# all pseudos: nicoi gr1+gr2
+#rag1spp <- nic12$delimitation[match(labels(rag1pseud), nic12$catalogNumber)]# all pseudos: nicoi gr1+gr2
 #
-cytbspp <- nic1nsp$species[match(labels(cytbpseud), nic1nsp$code)]# all pseudos: nicoi gr1+n.sp.
-rag1spp <- nic1nsp$species[match(labels(rag1pseud), nic1nsp$code)]# all pseudos: nicoi gr1+n.sp.
+#cytbspp <- nic1nsp$delimitation[match(labels(cytbpseud), nic1nsp$catalogNumber)]# all pseudos: nicoi gr1+n.sp.
+#rag1spp <- nic1nsp$delimitation[match(labels(rag1pseud), nic1nsp$catalogNumber)]# all pseudos: nicoi gr1+n.sp.
 #
-cytbspp <- nic12nsp$species[match(labels(cytbpseud), nic12nsp$code)]# all pseudos: nicoi gr1+gr2+n.sp.
-rag1spp <- nic12nsp$species[match(labels(rag1pseud), nic12nsp$code)]# all pseudos: nicoi gr1+gr2+n.sp.
+#cytbspp <- nic12nsp$delimitation[match(labels(cytbpseud), nic12nsp$catalogNumber)]# all pseudos: nicoi gr1+gr2+n.sp.
+#rag1spp <- nic12nsp$delimitation[match(labels(rag1pseud), nic12nsp$catalogNumber)]# all pseudos: nicoi gr1+gr2+n.sp.
+
 #
-cytbspp <- ttab$species[match(labels(cytbnic), ttab$code)]# just nicoi: 3 spp. delim
-rag1spp <- ttab$species[match(labels(rag1nic), ttab$code)]# just nicoi: 3 spp. delim
+cytbspp <- ttab$delimitation[match(labels(cytbnic), ttab$catalogNumber)]# just nicoi: 3 spp. delim
+rag1spp <- ttab$delimitation[match(labels(rag1nic), ttab$catalogNumber)]# just nicoi: 3 spp. delim
 #
-cytbspp <- nic12$species[match(labels(cytbnic), nic12$code)]# just nicoi: nicoi gr1+gr2
-rag1spp <- nic12$species[match(labels(rag1nic), nic12$code)]# just nicoi: nicoi gr1+gr2
+cytbspp <- nic12$delimitation[match(labels(cytbnic), nic12$catalogNumber)]# just nicoi: nicoi gr1+gr2
+rag1spp <- nic12$delimitation[match(labels(rag1nic), nic12$catalogNumber)]# just nicoi: nicoi gr1+gr2
 #
-cytbspp <- nic1nsp$species[match(labels(cytbnic), nic1nsp$code)]# just nicoi: nicoi gr1+n.sp.
-rag1spp <- nic1nsp$species[match(labels(rag1nic), nic1nsp$code)]# just nicoi: nicoi gr1+n.sp.
+cytbspp <- nic1nsp$delimitation[match(labels(cytbnic), nic1nsp$catalogNumber)]# just nicoi: nicoi gr1+n.sp.
+rag1spp <- nic1nsp$delimitation[match(labels(rag1nic), nic1nsp$catalogNumber)]# just nicoi: nicoi gr1+n.sp.
 #
 #To view the nucleotide values 
-lapply(nucDiag(cytbpseud, cytbspp), length)
-lapply(nucDiag(rag1pseud, rag1spp), length)
+#lapply(nucDiag(cytbpseud, cytbspp), length)
+#lapply(nucDiag(rag1pseud, rag1spp), length)
 #
 lapply(nucDiag(cytbnic, cytbspp), length)
 lapply(nucDiag(rag1nic, rag1spp), length)
@@ -68,51 +71,57 @@ mlik <- optim.pml(mlm, optNni=TRUE, optGamma=TRUE, optInv=FALSE, model="HKY") # 
 mlm <- pml(pars, dat, k=4, inv=0, model="K80") # rag1
 mlik <- optim.pml(mlm, optNni=TRUE, optGamma=TRUE, optInv=FALSE, model="K80") # rag1
 # copy trees
+# cytb
 cytbtr <- midpoint(mlik$tree)
-cytbtr.nic12 <- cytbtr
-cytbtr.nic12nsp <- cytbtr
-cytbtr.nic1nsp <- cytbtr
+dcytbtr <- drop.tip(cytbtr, tip=cytbtr$tip.label[match(ttab$catalogNumber[grep("ancistrus", ttab$genus, ignore.case=TRUE)], cytbtr$tip.label)])
+plot(dcytbtr)
+cytbtr.nic12 <- dcytbtr
+cytbtr.nic12nsp <- dcytbtr
+cytbtr.nic1nsp <- dcytbtr
+
+# rag1
 rag1tr <- midpoint(mlik$tree)
-rag1tr.nic12 <- rag1tr
-rag1tr.nic12nsp <- rag1tr
-rag1tr.nic1nsp <- rag1tr
+drag1tr <- drop.tip(rag1tr, tip=rag1tr$tip.label[match(ttab$catalogNumber[grep("ancistrus", ttab$genus, ignore.case=TRUE)], rag1tr$tip.label)])
+plot(drag1tr)
+rag1tr.nic12 <- drag1tr
+rag1tr.nic12nsp <- drag1tr
+rag1tr.nic1nsp <- drag1tr
 
 
 ## rosenberg analysis (cytb)
-cytbros <- rosenberg(cytbtr)
+cytbros <- rosenberg(dcytbtr)
 # create the different trees with sp delims
-cytbtr$tip.label <- ttab$species[match(cytbtr$tip.label, ttab$code)]
-cytbtr.nic12$tip.label <- nic12$species[match(cytbtr.nic12$tip.label, nic12$code)]
-cytbtr.nic12nsp$tip.label <- nic12nsp$species[match(cytbtr.nic12nsp$tip.label, nic12nsp$code)]
-cytbtr.nic1nsp$tip.label <- nic1nsp$species[match(cytbtr.nic1nsp$tip.label, nic1nsp$code)]
+dcytbtr$tip.label <- ttab$delimitation[match(dcytbtr$tip.label, ttab$catalogNumber)]
+cytbtr.nic12$tip.label <- nic12$delimitation[match(cytbtr.nic12$tip.label, nic12$catalogNumber)]
+cytbtr.nic12nsp$tip.label <- nic12nsp$delimitation[match(cytbtr.nic12nsp$tip.label, nic12nsp$catalogNumber)]
+cytbtr.nic1nsp$tip.label <- nic1nsp$delimitation[match(cytbtr.nic1nsp$tip.label, nic1nsp$catalogNumber)]
 # extract the rosenberg P values
-cytbros[names(cytbros) == getMRCA(cytbtr, tip="n. sp.")]
-cytbros[names(cytbros) == getMRCA(cytbtr, tip="nicoi.gr1")]
-cytbros[names(cytbros) == getMRCA(cytbtr, tip="nicoi.gr2")]
+cytbros[names(cytbros) == getMRCA(dcytbtr, tip="n.sp.")]
+cytbros[names(cytbros) == getMRCA(dcytbtr, tip="nicoi.gr1")]
+cytbros[names(cytbros) == getMRCA(dcytbtr, tip="nicoi.gr2")]
 cytbros[names(cytbros) == getMRCA(cytbtr.nic12, tip="nicoi.gr1+gr2")]
-cytbros[names(cytbros) == getMRCA(cytbtr.nic12nsp, tip="nicoi.gr1+gr2+n.sp.")]
+#cytbros[names(cytbros) == getMRCA(cytbtr.nic12nsp, tip="nicoi.gr1+gr2+n.sp.")]
 cytbros[names(cytbros) == getMRCA(cytbtr.nic1nsp, tip="nicoi.gr1+n.sp.")]
 
 ## rosenberg analysis (rag1)
-rag1ros <- rosenberg(rag1tr)
+rag1ros <- rosenberg(drag1tr)
 # create the different trees with sp delims
-rag1tr$tip.label <- ttab$species[match(rag1tr$tip.label, ttab$code)]
-rag1tr.nic12$tip.label <- nic12$species[match(rag1tr.nic12$tip.label, nic12$code)]
-rag1tr.nic12nsp$tip.label <- nic12nsp$species[match(rag1tr.nic12nsp$tip.label, nic12nsp$code)]
-rag1tr.nic1nsp$tip.label <- nic1nsp$species[match(rag1tr.nic1nsp$tip.label, nic1nsp$code)]
+drag1tr$tip.label <- ttab$delimitation[match(drag1tr$tip.label, ttab$catalogNumber)]
+rag1tr.nic12$tip.label <- nic12$delimitation[match(rag1tr.nic12$tip.label, nic12$catalogNumber)]
+rag1tr.nic12nsp$tip.label <- nic12nsp$delimitation[match(rag1tr.nic12nsp$tip.label, nic12nsp$catalogNumber)]
+rag1tr.nic1nsp$tip.label <- nic1nsp$delimitation[match(rag1tr.nic1nsp$tip.label, nic1nsp$catalogNumber)]
 # extract the rosenberg P values
-rag1ros[names(rag1ros) == getMRCA(rag1tr, tip="n. sp.")]
+rag1ros[names(rag1ros) == getMRCA(drag1tr, tip="n.sp.")]
 #rag1ros[names(rag1ros) == getMRCA(rag1tr, tip="nicoi.gr1")] # not monophyletic
 #rag1ros[names(rag1ros) == getMRCA(rag1tr, tip="nicoi.gr2")] # not monophyletic
 rag1ros[names(rag1ros) == getMRCA(rag1tr.nic12, tip="nicoi.gr1+gr2")]
-rag1ros[names(rag1ros) == getMRCA(rag1tr.nic12nsp, tip="nicoi.gr1+gr2+n.sp.")]
 #rag1ros[names(rag1ros) == getMRCA(rag1tr.nic1nsp, tip="nicoi.gr1+n.sp.")] # not monophyletic
 
-#is.monophyletic(midpoint(mlik$tree), tips=c("T027","T151","T078","T149","T153","P18073","P18043","P18038","P152","P4647"))
-plot.phylo(rag1tr, use.edge.length=FALSE)
-write.tree(rag1tr.nic12, file="../temp/12.nwk")
+# check for monophyly
+plot.phylo(rag1tr.nic12, use.edge.length=FALSE)
+is.monophyletic(phy=rag1tr.nic12, tips=which(rag1tr.nic12$tip.label == "nicoi.gr1+gr2"))
 
-
+### DO BOOTSTRAP or POST PROBS ???
 
 ## gsi analysis
 # cytb
