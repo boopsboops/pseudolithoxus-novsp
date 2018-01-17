ebda636548e6a690ebacc48eb202e3c21ecd09d1
rupert@sorubim <rupertcollins@gmail.com>
2018-01-17 19:24:18 +0000

    added the new genbank accessions

--- scripts/genbank_submit.R
+++ scripts/genbank_submit.R
@@ -15,6 +15,7 @@ library("stringr")
 mol.tab <- read_csv(file="../data/mol_samples.csv")
 glimpse(mol.tab)
 
+# load nex
 cytb.nex <- as.DNAbin(read.nexus.data(file="../data/cytb_aligned.nex"))
 length(cytb.nex)
 rag1.nex <- as.DNAbin(read.nexus.data(file="../data/rag1_aligned.nex"))
@@ -34,12 +35,6 @@ setdiff(mol.tab$"otherCatalogNumbers", union(names(cytb.nex),names(rag1.nex)))
 mol.tab.gb <- mol.tab %>% filter(associatedSequences!="TBC")# or filter(remarks=="genbank")
 summary(mol.tab.gb$"otherCatalogNumbers" %in% names(cytb.nex))
 summary(mol.tab.gb$"otherCatalogNumbers" %in% names(rag1.nex))
-
-
-# get cytb accs
-str_split_fixed(string=mol.tab.gb$associatedSequences, pattern=" \\| ", n=2)[,1]
-# get rag1 accs
-str_split_fixed(string=mol.tab.gb$associatedSequences, pattern=" \\| ", n=2)[,2]
 
 # remove the already accessioned data from the DNA files 
 cytb.nex.sub <- cytb.nex[which(!names(cytb.nex) %in% mol.tab.gb$"otherCatalogNumbers")]
@@ -86,3 +81,35 @@ write(gb.feat.rag1, file="../temp2/rag1/features.tbl", append=FALSE)# write out
 # to run tbl2asn in terminal
 #./tbl2asn -t template.sbt -i sequences.fsa -f features.tbl -a s -V vb -T
 
+## Once done, add the accessions and make a Supplementary table
+
+
+# add the accessions from GenBank to spreadsheet
+cytb.acc <- read_csv(file="../temp2/cytb/CYTB.acc")
+rag1.acc <- read_csv(file="../temp2/rag1/RAG1.acc")
+
+# join the cytb and rag1
+mol.tab$tempcytb<-rep(NA, length(dim(mol.tab)[1]))
+mol.tab$temprag1<-rep(NA, length(dim(mol.tab)[1]))
+mol.tab$tempcytb[match(cytb.acc$code, mol.tab$otherCatalogNumbers)] <- cytb.acc$acc
+mol.tab$temprag1[match(rag1.acc$code, mol.tab$otherCatalogNumbers)] <- rag1.acc$acc
+cyt.rag <- paste(mol.tab$tempcytb, mol.tab$temprag1, sep=" | ")
+cyt.rag[which(mol.tab$associatedSequences!="TBC")] <- mol.tab$associatedSequences[which(mol.tab$associatedSequences!="TBC")]
+mol.tab$associatedSequences <- cyt.rag
+
+# see cytb accs
+str_split_fixed(string=mol.tab$associatedSequences, pattern=" \\| ", n=2)[,1]
+# see rag1 accs
+str_split_fixed(string=mol.tab$associatedSequences, pattern=" \\| ", n=2)[,2]
+
+# filter the DwC data
+suppl.tab <- mol.tab %>% #
+    mutate(scientificName=ifelse(test=is.na(identificationQualifier), yes=paste(genus,specificEpithet), no=paste(genus,identificationQualifier))) %>% #
+    select(otherCatalogNumbers,associatedSequences,catalogNumber,institutionCode,collectionCode,basisOfRecord,#
+    typeStatus,scientificName,kingdom,phylum,class,order,family,genus,specificEpithet,scientificNameAuthorship,identificationQualifier,
+    taxonRank,identifiedBy,decimalLatitude,decimalLongitude,country,stateProvince,waterBody,eventDate,locality,recordedBy)
+#    
+dim(suppl.tab)
+
+# write out 
+write_csv(suppl.tab, path="../manuscript/tables/Supplementary_Table_S2.csv")
